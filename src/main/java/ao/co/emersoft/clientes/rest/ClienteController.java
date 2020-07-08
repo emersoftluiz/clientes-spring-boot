cb3f7d1c0d122ce82b44f7f77019de7c1ad74af8
Emersoft <emersonbsi@gmail.com>
2020-07-08 16:30:29 -0300

    Atualizando um Cliente

--- src/main/java/ao/co/emersoft/clientes/rest/ClienteController.java
+++ src/main/java/ao/co/emersoft/clientes/rest/ClienteController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -44,5 +45,15 @@ public class ClienteController {
 			repository.delete(cliente);
 			return Void.TYPE;
 		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
+	
+	@PutMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void alterar(@PathVariable Integer id, @RequestBody Cliente clienteAtualizado) {
+		repository.findById(id).map(cliente -> {
+			cliente.setNome(clienteAtualizado.getNome());
+			cliente.setCpf(clienteAtualizado.getCpf());
+			return repository.save(cliente);
+		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));		
 	}
 }
