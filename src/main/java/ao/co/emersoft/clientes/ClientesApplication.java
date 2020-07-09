1610374106bab04ff22136305f97541d31d198b5
Emersoft <emersonbsi@gmail.com>
2020-07-08 21:24:58 -0300

    Concluindo a validação de clientes

--- src/main/java/ao/co/emersoft/clientes/ClientesApplication.java
+++ src/main/java/ao/co/emersoft/clientes/ClientesApplication.java
@@ -1,24 +1,18 @@
 package ao.co.emersoft.clientes;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import ao.co.emersoft.clientes.model.entity.Cliente;
-import ao.co.emersoft.clientes.model.repository.ClienteRepository;
 
 @SpringBootApplication
 public class ClientesApplication {
-	
+	/*
 	@Bean
 	public CommandLineRunner run(@Autowired ClienteRepository repository) {
 		return args -> {
 			Cliente cliente = new Cliente(null, "Maria Brown", "12345678901", null);
 			repository.save(cliente);
 		};
-	}
+	}*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientesApplication.class, args);
